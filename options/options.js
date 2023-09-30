d99bd9ac5895fa35b28ab00a2d5130f25e5109a2
Crowdbotics <team@crowdbotics.com>
2023-09-30 07:04:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "theliquorcabinet-43674",
+  url: "https://theliquorcabinet-43674.botics.co",
+  api: "https://theliquorcabinet-43674.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
